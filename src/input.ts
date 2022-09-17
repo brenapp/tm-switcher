335e115f2cd431816264d297ccce63858d2764a0
Brendan McGuire <brendan@bren.app>
2022-09-16 22:30:12 -0400

    Refactor: move input to own file

---
+++ src/input.ts
@@ -0,0 +1,103 @@
+import inquirer from "inquirer";
+import ObsWebSocket from "obs-websocket-js";
+import Client from "vex-tm-client";
+import { join } from "path";
+import { promises as fs } from "fs";
+import { cwd } from "process";
+import Fieldset from "vex-tm-client/out/Fieldset";
+
+export async function getFieldset(tm: Client) {
+    const response: { fieldset: string } = await inquirer.prompt([
+        {
+            name: "fieldset",
+            type: "list",
+            message: "Which fieldset do you wish to control? ",
+            choices: tm.fieldsets.map((d) => d.name),
+        },
+    ]);
+
+    return tm.fieldsets.find(
+        (set) => set.name === response.fieldset
+    ) as Fieldset;
+};
+
+export async function getAssociations(fieldset: Fieldset, obs: ObsWebSocket) {
+    const fields = fieldset.fields;
+    const scenes = await obs.call("GetSceneList");
+
+    const associations: string[] = [];
+
+    for (const field of fields) {
+        const response = await inquirer.prompt([
+            {
+                name: "scene",
+                type: "list",
+                message: `What scene do you want to associate with ${field.name}? `,
+                choices: scenes.scenes.map((s) => s.sceneName),
+            },
+        ]);
+        associations[field.id] = response.scene;
+    };
+
+    return associations;
+};
+
+export async function getAudienceDisplayOptions() {
+
+    type Choices = "queueIntro" | "preventSwitch" | "savedScore" | "rankings";
+    const choices: { name: string; value: Choices }[] = [
+        { name: "Show intro upon match queue", value: "queueIntro" },
+        { name: "Prevent switching display mode in-match", value: "preventSwitch" },
+        { name: "Show saved score 3 seconds after match", value: "savedScore" },
+        { name: "Flash rankings 3 seconds after every 6th match", value: "rankings" }
+    ];
+
+    const response: { options: Choices[] } = await inquirer.prompt([
+        {
+            name: "options",
+            type: "checkbox",
+            message: "Which audience display automation would you like to enable?",
+            choices
+        }
+    ]);
+
+    const flags = Object.fromEntries(choices.map(ch => [ch.value, false])) as Record<Choices, boolean>;
+    for (const option of response.options) {
+        flags[option] = true;
+    };
+
+    return flags;
+};
+
+export async function getRecordingPath(tm: Client): Promise<fs.FileHandle | undefined> {
+
+    const response: { record: boolean } = await inquirer.prompt([
+        {
+            name: "record",
+            type: "confirm",
+            message: "Save a file with stream timestamps for each match?"
+        }
+    ]);
+
+    if (response.record) {
+        const directory = cwd();
+
+        const date = new Date();
+        const path = join(directory, `tm_obs_switcher_${date.getDate()}_${date.getMonth() + 1}_${date.getFullYear()}_times.csv`);
+
+        console.log(`  Will save match stream times to ${path}`);
+        const handle = await fs.open(path, "a");
+
+        const stat = await handle.stat();
+
+        if (stat.size > 0) {
+            console.log(`  File already exists, will append new entries...`);
+        } else {
+            handle.write("TIMESTAMP,OBS_TIME,MATCH\n");
+        }
+
+        return handle
+    } else {
+        return undefined;
+    }
+};

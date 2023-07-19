8bdb2a386ce3e7ce6c5d7160a8a5b3267acdbe66
Brendan McGuire <brendan@bren.app>
2023-07-19 16:03:53 -0500

    Use new API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { V3MatchTuple, V3MatchTupleSkillsType } from "vex-tm-client";
 import { getCredentials, connectTM, connectOBS, connectATEM } from "./authenticate";
-import { AudienceDisplayMode, AudienceDisplayOptions } from "vex-tm-client/out/Fieldset";
-import { getAssociations, getAudienceDisplayOptions, getFieldset, getRecordingOptions } from "./input";
+import { getAssociations, getFieldset, getRecordingOptions } from "./input";
 
 (async function () {
 
@@ -29,22 +29,18 @@ import { getAssociations, getAudienceDisplayOptions, getFieldset, getRecordingOp
 
   // Configuration
   const fieldset = await getFieldset(tm);
-  const fields = new Map(fieldset.fields.map((f) => [f.id, f]));
   const associations = await getAssociations(fieldset, obs, atem);
 
   console.log("");
 
-  const audienceDisplayOptions = await getAudienceDisplayOptions();
+  // const audienceDisplayOptions = await getAudienceDisplayOptions();
   const { handle: timestampFile, recordIndividualMatches, division } = await getRecordingOptions(tm, obs);
 
   console.log("");
 
-  let queued = "";
-  let started = false;
-  let matchCount = 0;
+  let currentMatch: V3MatchTuple | null = null;
 
-  fieldset.ws.on("message", async (data) => {
-    const message = JSON.parse(data.toString());
+  async function getCurrentTimecode() {
     const recordStatus = await obs?.call("GetRecordStatus");
     const streamStatus = await obs?.call("GetStreamStatus");
 
@@ -57,158 +53,100 @@ import { getAssociations, getAudienceDisplayOptions, getFieldset, getRecordingOp
       timecode = streamStatus.outputTimecode;
     };
 
-    /**
-     * When a match is queued, switch to its associated scene
-     **/
-    async function fieldMatchAssigned() {
-      const id = message.fieldId;
+    return timecode;
+  };
 
-      // Elims won't have an assigned field, they can switch early, but we'll
-      // switch when the match starts
-      if (!id) {
-        return;
+  function matchTupleToString(match: V3MatchTuple | null) {
+    if (!match) return "Match";
+
+    switch (match.round) {
+      case "PRACTICE":
+      case "ROUND_ROBIN":
+      case "NONE":
+      case "TOP_N":
+      case "F":
+      case "QUAL": {
+        return `${match.round} ${match.match}`;
+      }
+      case "R128":
+      case "R64":
+      case "R32":
+      case "R16":
+      case "QF":
+      case "SF": {
+        return `${match.round} ${match.instance}-${match.match}`;
       }
 
-      let name = fields.get(id)?.name;
-      if (!name) {
-        name = "Unknown";
+      case "SKILLS": {
+        const skills: V3MatchTupleSkillsType[] = ["NO_SKILLS", "PROGRAMMING", "DRIVER"];
+        return `${skills?.[match.instance!] ?? ""} ${match.round}`
       };
 
-      const association = associations[id];
-      console.log(`[${new Date().toISOString()}] [${timecode}] info: ${message.name} queued on ${name}, switching to scene ${association.obs}${association.atem ? ` and ATEM ${association.atem}` : ""}`);
-
-      if (obs && association.obs) {
-        await obs.call("SetCurrentProgramScene", { sceneName: association.obs });
+      case "TIMEOUT": {
+        return `${match.round}`;
       }
 
-      if (atem && association.atem) {
-        atem.changeProgramInput(association.atem);
-      };
-
-      if (audienceDisplayOptions.queueIntro) {
-        fieldset.setScreen(AudienceDisplayMode.INTRO);
+      default: {
+        return JSON.stringify(match);
       }
-
-      // keep track of the queued match
-      queued = message.name;
-      started = false;
     };
 
-    /**
-     * When the match starts, switch the scene to the appropriate field, and log to the timestamp
-     * file and console 
-     **/
-    async function matchStarted() {
-      const id = message.fieldId;
+  };
 
-      let name = fields.get(id)?.name;
-      if (!name) {
-        name = "Unknown";
-      };
-
-      const association = associations[id];
-      console.log(`[${new Date().toISOString()}] [${timecode}] info: match ${started ? "resumed" : "started"} on ${name}, switching to scene ${association.obs}${association.atem ? ` and ATEM ${association.atem}` : ""}`);
-
-      if (obs && association.obs) {
-        await obs.call("SetCurrentProgramScene", { sceneName: association.obs });
-      }
-
-      if (atem && association.atem) {
-        atem.changeProgramInput(association.atem);
-      };
-
-      if (!started) {
-        started = true;
-
-        // Get information about the match
-        await division.refresh();
-        const match = division.matches.find((m) => m.name === queued);
-
-        if (obs && recordIndividualMatches) {
-          await obs.call("StartRecord");
-        }
-
-        await timestampFile?.write(`${new Date().toISOString()},${timecode},${queued},${match?.redTeams.join(" ")},${match?.blueTeams.join(" ")}\n`);
-
-      }
-    }
-
-    /**
-     * Every second during the match, prevent switching
-     **/
-    async function timeUpdated() {
-      // Force the audience display to be in-match during the entire match
-      if (audienceDisplayOptions.preventSwitch) {
-        fieldset.setScreen(AudienceDisplayMode.IN_MATCH);
-      };
-    }
-
-    /**
-     * When the match ends, switch to the end graphic (if appropriate)
-     **/
-    async function matchStopped() {
-      matchCount++;
-
-      // Show saved score 3 seconds after the match ends
-      if (audienceDisplayOptions.rankings && matchCount % 6 == 0) {
-        setTimeout(() => {
-          console.log(`[${new Date().toISOString()}] info: switching audience display to Rankings`);
-          fieldset.setScreen(AudienceDisplayMode.RANKINGS);
-        }, 3000);
-      } else if (audienceDisplayOptions.savedScore) {
-        setTimeout(() => {
-          console.log(`[${new Date().toISOString()}] info: switching audience display to Saved Match Results`);
-          fieldset.setScreen(AudienceDisplayMode.SAVED_MATCH_RESULTS);
-        }, 3000);
-      };
-
-      if (obs && recordIndividualMatches) {
-        await obs.call("StopRecord");
-      };
-    };
-
-    /**
-     * Disable showing saved score/rankings when the display switches to elim bracket or alliance selection
-     **/
-    async function displayUpdated() {
-      const mode = message.display as AudienceDisplayMode;
-      const option = message.displayOption as AudienceDisplayOptions;
-
-      // Disable show saved score when you switch display to elim bracket or alliance selection
-      if (mode == AudienceDisplayMode.ELIM_BRACKET || mode == AudienceDisplayMode.ALLIANCE_SELECTION) {
-        if (audienceDisplayOptions.savedScore) {
-          console.log(`[${new Date().toISOString()}] info: detected switch to ${AudienceDisplayMode[mode]}, disabling show saved score & rankings automation`);
-          audienceDisplayOptions.savedScore = false;
-          audienceDisplayOptions.rankings = false;
-        };
-      };
-
-    };
-
-    // Dispatch each event appropriately
-    switch (message.type) {
-      case "fieldMatchAssigned": {
-        await fieldMatchAssigned();
-        break;
-      }
-      case "matchStarted": {
-        await matchStarted();
-        break;
-      }
-      case "timeUpdated": {
-        await timeUpdated();
-        break;
-      }
-      case "matchStopped": {
-        await matchStopped();
-        break;
-      }
-      case "displayUpdated": {
-        await displayUpdated();
-        break;
-      }
+  fieldset.on("ASSIGN_FIELD_MATCH", async message => {
+    if (message.match) {
+      currentMatch = message.match;
     }
   });
+
+  // Switch the scene upon field activation (this is better than queuing for events with V5 field control)
+  fieldset.on("FIELD_ACTIVATED", async message => {
+    const field = message.fieldId;
+    if (!field) return;
+
+    const timecode = await getCurrentTimecode();
+    const association = associations[field];
+
+    console.log(`[${new Date().toISOString()}] [${timecode}] info: ${matchTupleToString(currentMatch)} queued on ${fieldset.fields[field]}${association.obs ? ", switching to scene " + association.obs : ""}${association.atem ? `, ATEM ${association.atem}` : ""}`);
+
+    if (obs && association.obs) {
+      await obs.call("SetCurrentProgramScene", { sceneName: association.obs });
+    }
+
+    if (atem && association.atem) {
+      atem.changeProgramInput(association.atem);
+    };
+
+  });
+
+  // Switch the scene upon field activation (this is better than queuing for events with V5 field control)
+  fieldset.on("MATCH_STARTED", async message => {
+    const field = message.fieldId;
+    if (!field) return;
+
+    const timecode = await getCurrentTimecode();
+    const association = associations[field];
+
+    console.log(`[${new Date().toISOString()}] [${timecode}] info: ${matchTupleToString(currentMatch)} started on ${fieldset.fields[field]}${association.obs ? ", switching to OBS scene " + association.obs : ""}${association.atem ? ` ATEM ${association.atem}` : ""}`);
+
+    if (obs && association.obs) {
+      await obs.call("SetCurrentProgramScene", { sceneName: association.obs });
+    }
+
+    if (atem && association.atem) {
+      atem.changeProgramInput(association.atem);
+    };
+
+    // Record the match
+    if (obs && recordIndividualMatches) {
+      await obs.call("StartRecord");
+    };
+
+    // Write the timestamp file
+    await timestampFile?.write(`${new Date().toISOString()},${timecode},${matchTupleToString(currentMatch)}\n`);
+
+  });
+
 })();
 
 process.on("exit", () => {

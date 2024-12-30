35f067ccae51b27485cdcb573819abaf6e36fe07
Brendan McGuire <brendan@bren.app>
2024-12-29 22:51:39 -0600

    Inject secrets into bundled rollup at build time

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { typescriptPaths } from "rollup-plugin-typescript-paths";
-import dotenv from "rollup-plugin-dotenv";
+import injectProcessEnv from "rollup-plugin-inject-process-env";
+
+import "dotenv/config.js";
 
 /**
  * @type {import('rollup').RollupOptions}
@@ -24,7 +26,15 @@ const config = {
     commonjs({
       ignoreDynamicRequires: true,
     }),
-    dotenv(),
+    injectProcessEnv({
+      TM_SWITCHER_VEX_TM_CLIENT_ID: process.env.TM_SWITCHER_VEX_TM_CLIENT_ID,
+      TM_SWITCHER_VEX_TM_CLIENT_SECRET:
+        process.env.TM_SWITCHER_VEX_TM_CLIENT_SECRET,
+      TM_SWITCHER_VEX_TM_EXPIRATION_DATE:
+        process.env.TM_SWITCHER_VEX_TM_EXPIRATION_DATE,
+      TM_SWITCHER_LOG_SERVER: process.env.TM_SWITCHER_LOG_SERVER,
+      TM_SWITCHER_LOG_TOKEN: process.env.TM_SWITCHER_LOG_TOKEN,
+    }),
   ],
   external: ["@julusian/freetype2", "atem-connection"],
   output: {

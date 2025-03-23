828302890fe3c0283e37d60b99a943fba8fe2cbe
Brendan McGuire <brendan@bren.app>
2025-03-22 20:50:34 -0500

    tauri re-init

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
-import tailwindcss from "tailwindcss"
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  plugins: [
-    
-  ],
+  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,

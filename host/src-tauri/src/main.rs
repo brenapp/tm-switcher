006ca6d5014493c688a1d675a6f353ed8b1ed1d1
Brendan McGuire <brendan@bren.app>
2025-03-26 23:08:55 -0500

    transparent title bar on Mac OS

--- host/src-tauri/src/main.rs
+++ host/src-tauri/src/main.rs
@@ -6,6 +6,7 @@ mod process;
 use anyhow::Result;
 use portable_pty::{CommandBuilder, PtySize};
 use process::{ProcessEvent, ProcessOptions};
+use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
 use std::io::Write;
 use std::process::Command;
 use std::sync::{Arc, Mutex};
@@ -58,6 +59,36 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![begin])
+        .setup(|app| {
+            let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+                .title("TM Switcher")
+                .inner_size(950.0, 600.0);
+            
+            // Transparent title bar on Mac OS
+            #[cfg(target_os = "macos")]
+            let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+            let window = win_builder.build().unwrap();
+
+            #[cfg(target_os = "macos")]
+            {
+                use cocoa::appkit::{NSColor, NSWindow};
+                use cocoa::base::{id, nil};
+
+                let ns_window = window.ns_window().unwrap() as id;
+                unsafe {
+                    let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                        nil,
+                        22.0 / 255.0,
+                        22.0 / 255.0,
+                        22.5 / 255.0,
+                        1.0,
+                    );
+                    ns_window.setBackgroundColor_(bg_color);
+                }
+            }
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
